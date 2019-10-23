8c8de45c526c73a8f26ab8676b6a8bb06acd1f6a
Annie Truex <artruex@gmail.com>
2019-10-23 17:24:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,14 @@ class App < Sinatra::Base
   end
 
     post "/teams" do
+<<<<<<< HEAD
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
       @super_heroes = members.collect do |member_params|
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
+=======
+>>>>>>> d49edafdf11b406e17c391d228d97cd5d90ce8df
       erb :team
     end
 end
